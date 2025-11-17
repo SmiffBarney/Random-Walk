fa8d017954dd03fa06417edddab00901907eeede
Sean <niezsean@gmail.com>
2025-11-16 22:08:33 -0500

    fixed readme

---
+++ cmake-build-debug/CMakeFiles/Random-Walk.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Random-Walk.dir/multiwalk.cpp.obj"
+  "CMakeFiles/Random-Walk.dir/multiwalk.cpp.obj.d"
+  "Random-Walk.exe"
+  "Random-Walk.exe.manifest"
+  "Random-Walk.pdb"
+  "libRandom-Walk.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Random-Walk.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
